8a9076fa1180a3168909b0637912888b9f50857d
Godstimeconst{sellername,price,description <okechukwugodstime75@gmail.com>
2026-03-15 20:22:45 +0100

    staticcommit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // This generates a static 'out' folder
+  images: {
+    unoptimized: true, // Required for static export if using next/image
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
